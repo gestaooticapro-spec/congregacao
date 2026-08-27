0759bfdb2bf30190c1207276284e0c2766951fa3
Jaime Rodrigues Jr <jaimerodriguesjunior@outlook.com>
2026-08-27 17:17:12 -0300

    altera identidade visual para amarelo solar

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
-        theme_color: '#ffffff',
+        theme_color: '#F2B705',
         icons: [
             {
                 src: '/icon.svg',
